7c8d1f2040ea91ce848251b0dc5d4c7dcb1a90f5
anwesht <anwesh.tuladhar@gmail.com>
2017-10-03 23:24:45 -0400

    Added plots to analyze-centrality. Finished problem 2 (analyze-connectivity)

--- assignment2/analyze-centrality.py
+++ assignment2/analyze-centrality.py
@@ -1,6 +1,7 @@
 import sys
 import networkx as nx
 import matplotlib.pyplot as plt
+from random import random
 
 
 class ShortestPathMetrics:
@@ -51,29 +52,43 @@ class ShortestPathMetrics:
 
     def get_closeness_centrality(self):
         """
-        Calculate the closeness centrality as in eqn 7.29 Newman:
-        C_i = n/sum of d_ij
+        Calculate the closeness centrality as in:
+        https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.closeness_centrality.html
+        C_i = ((n - 1)/(N-1)) * (n-1) / sum of d_ij
         where,
             n = number of nodes in the network,
             i = node for which the centrality is being calculated,
             j = all other nodes,
             d_ij = geodesic path from i to j (length of the path)
 
+        Using eqn 7.29 Newman:[ C_i = n/sum of d_ij ] results in 0.0 centrality due to disconnected components.
+
         :return: dictionary with key = node i, value = closeness centrality of i.
         :rtype: dict[int, float]
         """
         closeness = {}
-        n = nx.number_of_nodes(self.__g)
+        N = nx.number_of_nodes(self.__g)
 
         for i in nx.nodes(self.__g):
             shortest_paths_for_node = self.shortest_path_lengths[i]
+            n = len(shortest_paths_for_node)
+            # Newman's formula yields in 0 centrality.
+            # if len(shortest_paths_for_node) != n:
+            #     closeness[i] = 0.0
+            #     continue
+
             total_path_length = 0.0
 
             for j, l in shortest_paths_for_node.items():
                 if i != j:
-                    total_path_length += 1.0 / l
+                    total_path_length += l
 
-            closeness_centrality = n/total_path_length
+            # closeness_centrality = n/total_path_length  # Newman's formula
+            closeness_centrality = 0.0
+
+            if total_path_length > 0.0:
+                closeness_centrality = ((n - 1.0)/(N - 1.0)) * ((n - 1.0) / total_path_length)
+
             closeness[i] = closeness_centrality
 
         return closeness
@@ -106,7 +121,7 @@ def get_local_clustering_coef(g, n):
 
     for n1, n2 in all_pairs:
         if g.has_edge(n1, n2):
-            num_connected_neighbors += 1
+            num_connected_neighbors += 1.0
 
     c_i = num_connected_neighbors / len(all_pairs)
 
@@ -137,6 +152,76 @@ def write_centrality(name, centrality_dict):
     f.close()
 
 
+def is_equal(f1, f2):
+    precision = 0.000001  # considering upto 6 decimal places.
+    # precision = 0.00000000000001  # considering upto 6 decimal places.
+    # precision = 0.0001
+    return abs(f1 - f2) < precision
+
+
+def setup_log_log_plot(ax, x_axis_vals, y_axis_vals):
+    """
+    Sets up the axis values for a log-log plot.
+    :param ax:
+    :param x_axis_vals: list of values for x-axis
+    :param y_axis_vals: list of values for y-axis
+    :return: void
+    """
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    if len(y_axis_vals) == 1:
+        lo = float(y_axis_vals[0]) / 10
+        hi = float(y_axis_vals[0]) * 10
+        ax.set_ylim(ymin=lo, ymax=hi)
+    if len(x_axis_vals) == 1:
+        lo = float(x_axis_vals[0]) / 10
+        hi = float(x_axis_vals[0]) * 10
+        ax.set_xlim(xmin=lo, xmax=hi)
+
+
+def plot_centrality_from_file(file_path=None, is_log_plot=False):
+    if file_path is None:
+        print "No input file provided."
+
+    graph_name = file_path.split('/')[-1].split('.')[0]
+    centrality_name = file_path.split('/')[-1].split('.')[1]
+
+    debug("Plotting centrality %s for %s" % (graph_name, centrality_name))
+
+    ranked_centralities = {}
+    prev_centrality = random()
+    rank = 0
+
+    with open(file_path, 'r') as f:
+        for line in f:
+            l = line.strip().split(",")
+            if len(l) == 0:
+                break
+
+            centrality = float(l[1].strip())
+            if not is_equal(centrality, prev_centrality):
+                rank += 1
+                ranked_centralities[rank] = centrality
+
+    ranked_centralities = sorted(ranked_centralities.items())
+    x_axis_vals = [k for (k, _) in ranked_centralities]
+    y_axis_vals = [v for (_, v) in ranked_centralities]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    if is_log_plot is True:
+        setup_log_log_plot(ax, x_axis_vals, y_axis_vals)
+
+    plt.title("Plot of the {} for \n for {}".format(centrality_name, graph_name))
+    plt.xlabel("Rank")
+    plt.ylabel("Centrality")
+    ax.plot(x_axis_vals, y_axis_vals, '-r')
+
+    file_name = graph_name + "." + centrality_name + ".log-log.png" if is_log_plot else ".png"
+    fig.savefig(file_name)
+
+
 def debug(msg="Nothing to Print"):
     if DEBUG is True:
         print msg
@@ -164,13 +249,16 @@ def main(argv):
     debug("Finished degree centrality")
 
     # 2. Closeness Centrality
-    closeness = nx.closeness_centrality(g_nx)
-    debug("Finished closeness centrality")
+    # closeness = nx.closeness_centrality(g_nx)
+    # debug("Finished closeness centrality")
 
     # 3. Harmonic Centrality
     shortest_path_metrics = ShortestPathMetrics(g_nx)
     harmonic = shortest_path_metrics.get_harmonic_centrality()
     debug("Finished harmonic centrality")
+
+    closeness = shortest_path_metrics.get_closeness_centrality()
+    debug("Finished closeness centrality")
 
     # Uncomment to check un-normalized harmonic centrality
     # harmonic = nx.harmonic_centrality(g_nx)
@@ -192,5 +280,15 @@ def main(argv):
     write_centrality(graph_name + ".clustering.txt", clustering)
 
 
+def plot_centralities(argv, is_log_plot=False):
+    file_name = argv[0].split('/')[-1].split('.')[0]
+
+    plot_centrality_from_file(file_name + ".degree.txt", is_log_plot=is_log_plot)
+    plot_centrality_from_file(file_name + ".closeness.txt", is_log_plot=is_log_plot)
+    plot_centrality_from_file(file_name + ".harmonic.txt", is_log_plot=is_log_plot)
+    plot_centrality_from_file(file_name + ".betweenness.txt", is_log_plot=is_log_plot)
+    plot_centrality_from_file(file_name + ".clustering.txt", is_log_plot=is_log_plot)
+
 if __name__ == "__main__":
     main(sys.argv[1:])
+    # plot_centralities(sys.argv[1:], is_log_plot=True)
---
+++ assignment2/analyze_connectivity.py
@@ -0,0 +1,117 @@
+import sys
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import choice
+
+
+def get_local_clustering_coef(g, n):
+    """
+    This code was duplicated here from analyze-centrality.py because of the naming conventions used in the files.
+    file named with a hyphen "-" cannot be imported here.
+    """
+    neighbors = g.neighbors(n)
+    all_pairs = []
+
+    for i, n1 in enumerate(neighbors):
+        for n2 in neighbors[i:]:
+            all_pairs.append((n1, n2))
+
+    if len(all_pairs) == 0:
+        print "Zero local clustering coefficient"
+        return 0.0
+
+    num_connected_neighbors = 0.0
+
+    for n1, n2 in all_pairs:
+        if g.has_edge(n1, n2):
+            num_connected_neighbors += 1.0
+
+    c_i = num_connected_neighbors / len(all_pairs)
+
+    return c_i
+
+
+def debug(msg="Nothing to Print"):
+    if DEBUG is True:
+        print msg
+
+
+DEBUG = True
+
+
+def plot_distribution(distribution_dict, xlabel, ylabel, graph_name):
+    distribution_dict = sorted(distribution_dict.items())
+
+    debug("type of dict: {}".format(type(distribution_dict)))
+    x_axis_vals = [k for (k, _) in distribution_dict]
+    y_axis_vals = [v for (_, v) in distribution_dict]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    plt.title("Plot of the {} vs {} for \n for {}".format(xlabel, ylabel, graph_name))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    ax.plot(x_axis_vals, y_axis_vals, '-r')
+
+    file_name = graph_name + "." + xlabel + "_vs_" + ylabel + ".png"
+    fig.savefig(file_name)
+
+    return file_name
+
+
+def main(argv):
+    if len(argv) != 1:
+        print "usage: python analyze-centrality.py <path/to/edgelist>"
+        sys.exit(0)
+
+    graph_file_path = argv[0]
+    graph_name = graph_file_path.split('/')[-1].split('.')[0]
+
+    print "Current file: {}".format(graph_name)
+    print "nx version: {}".format(nx.__version__)
+
+    # 1. Read in the weighted edge lists file as an undirected graph with node type integer.
+    g_nx = nx.read_weighted_edgelist(path=graph_file_path, create_using=nx.Graph(), nodetype=int)
+
+    # Number of triads.
+    num_triads = len(nx.triangles(g_nx))
+    print "Number of triads: {}".format(num_triads)
+
+    # 2. Local Clustering coefficient of a randomly selected node
+    random_node = choice(nx.nodes(g_nx))
+    lcc = get_local_clustering_coef(g_nx, random_node)
+    print "Clustering coefficient of random node {} in {}: {}".format(random_node, graph_name, lcc)
+
+    # 3. Number of triads a random node participates in
+    random_node = choice(nx.nodes(g_nx))
+    num_triads_for_rand_node = nx.triangles(g_nx, random_node)
+    print "Number of triads node {} participates in {} triads".format(random_node, num_triads_for_rand_node)
+
+    # 4. Watts-Strogratz (average over local) and global clustering coefficients.
+    avg_clustering = nx.average_clustering(g_nx)
+    global_clustering_dict = nx.clustering(g_nx)
+    avg_global_clustering = sum(v for k, v in global_clustering_dict.items()) / len(global_clustering_dict)
+    print "Clustering coefficient of the network: {} (Watts-Strogatz); {} (global)".format(avg_clustering,
+                                                                                           avg_global_clustering)
+
+    # 5. Plot of the k-core edge-size distribution
+    k_core_edge_dict = {}
+    k_core_node_dict = {}
+    max_num_cores = max(nx.core_number(g_nx).values())
+    for i in range (0, max_num_cores):
+        k_core_edge_dict[i] = nx.k_core(g_nx, k=i).number_of_edges()
+        k_core_node_dict[i] = nx.k_core(g_nx, k=i).number_of_nodes()
+
+    file_name = plot_distribution(k_core_edge_dict, "core k", "number of edges in k-core", graph_name)
+
+    print "k-core edge-size distribution is in: {}".format(file_name)
+
+    # 6. Plot of the k-core node-size distribution
+    file_name = plot_distribution(k_core_edge_dict, "core k", "number of nodes in k-core", graph_name)
+
+    print "k-core node-size distribution is in: {}".format(file_name)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
