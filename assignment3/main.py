6f2f26d6a8bc9f5bc4d03bd2a2e96d46876d934f
anwesht <anwesh.tuladhar@gmail.com>
2017-11-02 15:51:27 -0400

    Assignment3: Made changes to plots.

--- assignment3/main.py
+++ assignment3/main.py
@@ -44,12 +44,15 @@ def plot_degree_distribution(g1, graphs):
     for g in graphs:
         degree_dict_list.append(get_degree_dict(g))
 
-    plt.title("Plot of the degree distribution")
+    # plt.title("Plot of the degree distribution")
     plt.xlabel("log (k)")   # Degree
     plt.ylabel("log (Pk)")  # frequency/ degree distribution
 
     fig = plt.figure(num_figures)
     num_figures += 1
+
+    fig.suptitle("Plot of the degree distribution")
+
     ax = fig.add_subplot(111)
     ax.set_xscale('log')
     ax.set_yscale('log')
@@ -69,8 +72,8 @@ def plot_degree_distribution(g1, graphs):
     fig.savefig("degree_distribution_facebook.png")
 
 
-def plot_degree_distribution_from_dict(degree_dict):
-    plt.title("Plot of the degree distribution from degree dict")
+def plot_degree_distribution_from_dict(name, degree_dict_by_p):
+    # plt.title("Plot of the degree distribution \nfor: {}".format(name))
     plt.xlabel("log (k)")   # Degree
     plt.ylabel("log (Pk)")  # frequency/ degree distribution
 
@@ -78,38 +81,49 @@ def plot_degree_distribution_from_dict(degree_dict):
 
     fig = plt.figure(num_figures)
     num_figures += 1
+    fig.suptitle("Plot of the degree distribution \nfor: {}".format(name))
+
     ax = fig.add_subplot(111)
     ax.set_xscale('log')
     ax.set_yscale('log')
 
-    degree_dict = sorted(degree_dict.items())
+    degree_dict_by_p = sorted(degree_dict_by_p.items())
 
     i = 0
-    x_axis_vals = [k for (k, _) in degree_dict]
-    y_axis_vals = [v for (_, v) in degree_dict]
+    for (p, degree_dict) in degree_dict_by_p:
+        x_axis_vals = [k for (k, _) in degree_dict.items()]
+        y_axis_vals = [v for (_, v) in degree_dict.items()]
 
-    ax.plot(x_axis_vals, y_axis_vals, color=COLORS[i])
+        ax.plot(x_axis_vals, y_axis_vals, color=COLORS[i], label=p)
 
-    fig.savefig("degree-distribution-from-dict")
+        i = (i + 1) % len(COLORS)
+
+    ax.legend(loc='best')
+    fig.savefig(name)
 
 
-def plot_assortativity(m_dict):
+def plot_assortativity(name, m_dict):
     print "Plotting assortativity"
 
-    plt.title("Plot: Assortativity coefficient (r) vs p")
+    # plt.title("Plot: Assortativity coefficient (r) vs p")
     plt.xlabel("p")  # Probability of leader
     plt.ylabel("r")  # Assortativity
 
-    fig = plt.figure()
+    global num_figures
+    fig = plt.figure(num_figures)
+    fig.suptitle('Plot: Assortativity coefficient (r) vs p\n for: {}'.format(name))
+
+    num_figures += 1
+
     ax = fig.add_subplot(111)
 
     # m_dict = sorted(m_dict.items(), key=lambda t: t[0])
 
-    print "m_dict: {}".format(m_dict)
+    # print "m_dict: {}".format(m_dict)
     i = 0
     for (m, p_dict) in m_dict.items():
         p_dict = sorted(p_dict.items(), key=lambda t: t[0])
-        print "pdict: {}".format(p_dict)
+        # print "pdict: {}".format(p_dict)
         x_axis_vals = [k for (k, _) in p_dict]
         y_axis_vals = [v for (_, v) in p_dict]
 
@@ -117,15 +131,15 @@ def plot_assortativity(m_dict):
         i = (i + 1) % len(COLORS)
 
     ax.legend(loc='best')
-    # fig.savefig("assortativity.png")
+    fig.savefig(name)
 
 
-def get_output_file(graph_name, n, p, m, l):
+def get_output_file(graph_name, n, p, m, l, i):
     # return "{}{}/{}-n{}-p{}-m{}-v{}-edges.txt".format(OUTPUT_DIR, graph_name, graph_name, n, p, m, l)
-    return "{}-n{}-p{}-m{}-v{}-edges.txt".format(graph_name, n, p, m, l)
+    return "{}-n{}-p{}-m{}-v{}-iter{}-edges.txt".format(graph_name, n, p, m, l, i)
 
 
-def generate_graphs(graph_name, n0=10, n=100, l=1, m_list=[5], p_list=[0.0, 0.2, 0.4, 0.6, 0.8, 1.0]):
+def generate_graphs(graph_name, n0=10, n=100, l=1, m_list=[2, 5, 10], p_list=[0.0, 0.2, 0.4, 0.6, 0.8, 1.0]):
     # m_list = [5]
     # p_list = [0.0, 0.2, 0.4, 0.6]
     # m_list = [2, 5, 10]
@@ -133,16 +147,20 @@ def generate_graphs(graph_name, n0=10, n=100, l=1, m_list=[5], p_list=[0.0, 0.2,
 
     assortativities = {}
 
+    num_runs = 10
     graphs = []
-    avg_degree_dict = {}
+    avg_degree_dict_by_p = {}
     for m in m_list:
+        n0 = m
         p_r_dict = {}
         for p in p_list:
             sum_of_r = 0.0
-            for i in range(0, 20):
-                graph = Generator(n0=n0, n=n, l=1, m=m, p=p).generate()
+            avg_degree_dict = {}
+
+            for i in range(0, num_runs):
+                graph = Generator(n0=n0, n=n, l=l, m=m, p=p).generate()
                 # graph = Generator(m, n, 1, m, p).generate()
-                of = get_output_file(graph_name, n, p, m, l)
+                of = get_output_file(graph_name, n, p, m, l, i)
                 nx.write_edgelist(graph, of, data=False)
 
                 # draw(graph)
@@ -162,14 +180,20 @@ def generate_graphs(graph_name, n0=10, n=100, l=1, m_list=[5], p_list=[0.0, 0.2,
                 r = nx.degree_assortativity_coefficient(graph)
                 sum_of_r += r
             # ----
-            r_avg = sum_of_r/20
+            avg_degree_dict_by_p[p] = avg_degree_dict
+            r_avg = sum_of_r / num_runs
             p_r_dict[p] = r_avg
+
+        degree_distr_name = "{}_n0={}_n={}_l={}_m={}_degree_dist.png".format(graph_name, n0, n, l, m)
+        plot_degree_distribution_from_dict(degree_distr_name, avg_degree_dict_by_p)
+
         assortativities[m] = p_r_dict
 
     # plot_degree_distribution(graphs.pop(0), graphs)
-    plot_degree_distribution_from_dict(avg_degree_dict)
+    # plot_degree_distribution_from_dict(avg_degree_dict)
     print "Assortativities: {}".format(assortativities)
-    plot_assortativity(assortativities)
+    assortativities_name = "{}-n={}-assortativity.png".format(graph_name, n)
+    plot_assortativity(assortativities_name, assortativities)
 
 
 def main():
@@ -183,11 +207,12 @@ def main():
     Analyzer(facebook, "facebook").analyze()
 
     # draw(facebook)
-    plot_degree_distribution(facebook, [])
+    # plot_degree_distribution(facebook, [])
 
     arxiv = nx.read_edgelist(path=ARXIV, create_using=nx.Graph())
     Analyzer(arxiv, "arxiv").analyze()
 
+    plot_degree_distribution(arxiv, [])
     # analyzer.plot_degree_distribution(facebook)
 
     # generate_graphs(FACEBOOK.split('/')[-1].split('.')[0], facebook.number_of_nodes())
@@ -197,7 +222,10 @@ def main():
     # generate_graphs("fb107", n0=25, n=facebook.number_of_nodes(), l=1, m_list=[25], p_list=[0.75])
     # generate_graphs("fb107", n0=25, n=facebook.number_of_nodes(), l=1, m_list=[25])
     # generate_graphs("fb107", n0=25, n=facebook.number_of_nodes(), l=1, m_list=[25], p_list=[0.5])
-    generate_graphs("fb107", n0=30, n=facebook.number_of_nodes(), l=3, m_list=[25], p_list=[0.43])
+    # generate_graphs("fb107", n0=30, n=facebook.number_of_nodes(), l=3, m_list=[25], p_list=[0.43])  # saved this.
+    # generate_graphs("arxiv", n0=5, n=arxiv.number_of_nodes(), l=1, m_list=[3], p_list=[0.9])
+    for n in [1000, 500]:
+        generate_graphs("synth", n=n)
 
 
 if __name__ == "__main__":
